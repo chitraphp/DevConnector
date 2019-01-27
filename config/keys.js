c9b3b666427eb2ec1107354f85886b5c592b3066
chitraphp <chitra.atmakuru@gmail.com>
2019-01-26 19:18:26 -0800

    add routes.js

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: 'mongodb://chitra:chitrakalpana19@ds163044.mlab.com:63044/devconnector'
+}
--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
 const app = express();
+
+const db = require('./config/keys').mongoURI;
+mongoose
+  .connect(db)
+  .then(() => console.log('MongoDb connected'))
+  .catch(err => console.log(err));
+
 app.get('/',(req,res) => res.send('hello world'));
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
 const port = 8000;
 app.listen(port, () => console.log(`server is running on port ${port}`));
